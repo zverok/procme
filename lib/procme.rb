02224a6ecee177b94c69a0eb8b1eb884226b432c
zverok <zverok.offline@gmail.com>
2015-05-13 15:13:20 +0300

    It works!

--- lib/procme.rb
+++ lib/procme.rb
@@ -1,32 +1,34 @@
 # encoding: utf-8
+# ProcMe: DRY and clean blocks for your code
 module ProcMe
   def filter(hash)
-    ->(o){
-      hash.all?{|k,v| v === o.send(k)}
-    }
+    lambda do |o|
+      hash.all?{|k, v| v === o.send(k)} # rubocop:disable Style/CaseEquality
+    end
   end
 
   alias_method :fltr, :filter
 
   def set(hash)
-    ->(o){
-      hash.each{|k,v| o.send("#{k}=", v)}
+    lambda do |o|
+      hash.each{|k, v| o.send("#{k}=", v)}
       o
-    }
+    end
   end
 
   def call(hash)
-    ->(o){
-      hash.each{|k,v| o.send(k, *v)}
+    lambda do |o|
+      hash.each{|k, v| o.send(k, *v)}
       o
-    }
+    end
   end
 
   def get(*array)
-    ->(o){
+    lambda do |o|
       array.map{|v| o.send(*v)}
-    }
+    end
   end
 
-  extend self # no we can use both include ProcMe & no include approach
+  # now we can use both include ProcMe & no include approach
+  extend self # rubocop:disable Style/ModuleFunction
 end
---
+++ procme.gemspec
@@ -0,0 +1,29 @@
+Gem::Specification.new do |s|
+  s.name     = 'procme'
+  s.version  = '0.0.1'
+  s.authors  = ['Victor Shepelev']
+  s.email    = 'zverok.offline@gmail.com'
+  s.homepage = 'https://github.com/zverok/procme'
+
+  s.summary = 'Useful DRY proc-s for Ruby'
+  s.description = <<-EOF
+    ProcMe provides you with methods for DRY and clean processing of
+    enumerables.
+  EOF
+  s.licenses = ['MIT']
+
+  s.files = `git ls-files`.split($RS).reject do |file|
+    file =~ /^(?:
+    spec\/.*
+    |Gemfile
+    |Rakefile
+    |\.rspec
+    |\.gitignore
+    |\.rubocop.yml
+    |\.travis.yml
+    )$/x
+  end
+  s.require_paths = ["lib"]
+
+  s.add_development_dependency 'rubocop', '~> 0.30'
+end
